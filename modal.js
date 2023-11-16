4d3bb4d6f16b455a66932f3b516cac9085144b6a
Nathalie Mallet <nathalie.nathweb@gmail.com>
2023-11-16 11:50:08 +0100

    modif apparence message erreur

--- modal.js
+++ modal.js
@@ -1,218 +1,238 @@
 function editNav() {
-  let x = document.getElementById("myHeader");
-  if (x.className === "header") {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "header";
+    x.className = "topnav";
   }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const validation = document.forms["reserve"];
-
-const closeBtn = document.querySelectorAll(".close");
-
-let fname = document.getElementById("fname");
-let l_name = document.getElementById("last");
-let email = document.getElementById("email");
-let birthdate = document.getElementById("birthdate");
-let nberQuantity = document.getElementById("quantity");
-let BtnRadio = document.getElementsByName('location');
-let CheckCondition = document.getElementById('checkbox1');
-
-const thankMsg = document.getElementById('thankMsg');
-const thankBtn = document.getElementById('thankBtn');
+const formData = document.querySelectorAll(".formData");
+const formSignup = document.getElementsByName("reserve");
+const formFirst = document.getElementById("first");
+const formLast = document.getElementById("last");
+const formMail = document.getElementById("email");
+const formDate = document.getElementById("birthdate");
+const formTimes = document.getElementById("quantity");
+const formCity = document.getElementsByName("location");
+const formCondition = document.getElementById("checkbox1");
+const formSubmit = document.getElementsByClassName("btn-submit");
+const formError = document.getElementsByClassName("error");
+const modalClose = document.querySelectorAll(".close");
+const modalCloseButton = document.getElementsByClassName("btn-close");
+const modalSbm = document.getElementsByClassName("modal-submitted");
+const modalBody = document.getElementsByClassName("modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+modalClose[0].addEventListener("click", closeModal);
+modalCloseButton[0].addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalBody[0].style.display = "";
 }
 
-//Evénement 'click' sur bouton fermeture
-
-closeBtn.forEach((btn) => {btn.addEventListener("click", closeModal)});
-function closeModal(){
+// close modal form
+function closeModal() {
   modalbg.style.display = "none";
- }
-
-
-// function display msg error
-const setError = (element, message) => {
-  const formData =  element.parentElement;
-  const errorDisplay = formData.querySelector('.msgError');
-  errorDisplay.innerHTML  = message;
-  formData.classList.add("error")
-  formData.classList.remove("success")
+  modalSbm[0].style.display = "none";
 }
-// function input success
-const setSuccess = element => {
-  const formData =  element.parentElement;
-  const errorDisplay = formData.querySelector('.msgError');
-  errorDisplay.innerHTML  = '';
-  formData.classList.add("success")
-  formData.classList.remove("error")
-}
+// Ajout d'un gestionnaire d'événements à l'événement submit du formulaire
+formSignup[0].addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
-
- //Contrôle entrée prénom
-
- fname.addEventListener("input", () => {
-  const regexFname = /^[a-zA-Z]+(([- ])?[a-zA-Z])+$/;
-  console.log(fname.value);
-  if(fname.value == ''){
-    setError(fname, 'Veuillez renseigner le Prénom');
-    return false;
-}
-else if(!regexFname.test(fname.value)){
-  setError(fname, 'Veuillez entrer 2 caractères ou plus pour le champ du Prénom.')
-  return false;
-}else{
-    setSuccess(fname);
-    return true;
-}
-})
-//Contrôle entrée nom
-
-l_name.addEventListener("input", () => {
-  const regexLast = /^[a-zA-Z]+(([- ])?[a-zA-Z])+$/;
-  l_name.value = l_name.value.toUpperCase();
-  if(l_name.value == ''){
-    setError(l_name, 'Veuillez renseigner le Nom');
-    return false;
-  }else if(!regexLast.test(l_name.value)){
-      setError(l_name, 'Veuillez entrer 2 caractères ou plus pour le champ du Prénom.')
-      return false;
-  }else
-    setSuccess(l_name);
-    return true;
-})
-//Contrôle entrée email
-
-function validEmail(email) {
-  const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regexEmail.test(String(email).toLowerCase());
-};
-email.addEventListener("input", () => {
-  console.log(email.value);
-  if(email.value == ''){
-    setError(email, 'Veuillez renseigner votre email');
+  // Appelle la fonction validate pour effectuer toutes les vérifications
+  if (!validate()) {
+    // Si la validation échoue, n'exécute pas l'envoi du formulaire
     return false;
   }
- else if (!validEmail(email.value) ){
-    setError(email, 'Veuillez entrer une adresse e-mail valide.')
-    return false;
-  }else{
-      setSuccess(email);
-      return true;
-  }
-  })
-  //Contrôle entrée date de naissance
 
-  function validBirthday(birthdate) {
-    const dateRegex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
-    return dateRegex.test(birthdate);
-  }
-  birthdate.addEventListener("input", () => {
-    console.log(birthdate.value);
-     if(!validBirthday(birthdate.value)) {
-      setError(birthdate, 'Veuillez entrer une date valide');
-      return false;
-    }else{
-      setSuccess(birthdate);
-      return true;
+  // Si la validation réussit, continue avec l'envoi du formulaire
+  this.submit();
+});
+// validation
+function validate() {
+  console.log("Validating..."); 
+  //run all change function to make sure
+  changeFormFirst();
+  changeFormLast();
+  changeFormMail();
+  changeFormTimes();
+  changeFormCity();
+  changeFormCondition();
+
+  //confirm if everything is valide, if it is, opens the thanking modal
+  if (
+    (FirstNIsValid &&
+      LastNIsValid &&
+      MailIsValid &&
+      DateIsValid &&
+      TimesIsValid &&
+      CityIsValid &&
+      ConditionIsValid) == false
+      ) {
+        console.log("Validation failed!");  // Ajout de cette ligne
+        return false;
+      } else {
+        console.log("Validation successful!");  // Ajout de cette ligne
+        modalBody[0].style.display = "none";
+        modalSbm[0].style.display = "flex";
+        formSignup[0].reset();
+        return false;
+      }
     }
-    })
-//Contrôle nombre de participation
 
-nberQuantity.addEventListener("change", () => {
-  console.log(nberQuantity.value);
-if(nberQuantity.value == ''){
-setError(nberQuantity, 'Veuillez entrer une valeur entre 0 et 99');
-return false;
-}else{
-setSuccess(nberQuantity);
-return true;
-}
-})
-//Contrôle ville saisie
-
-let city = null;
-for (var i = 0; i < BtnRadio.length; i++) {
-  BtnRadio[i].addEventListener('change', function() {
-    city = this.value;
-   
-    if(city == ""){
-      setError(BtnRadio, 'Veuillez choisir une ville.')
-      return false;
-    }else{
-      return true;
-    }
-})
-};
-//Conditions d'utilisation
-
-function condition() {
-  let condition = this.value;
-  if (condition.checked) {
-    return true;
-  } 
-}
-
-// Validation du formulaire
-function isFormValid() {
-  if (!fname.value ||
-    !l_name.value ||
-    !email.value ||
-    !birthdate.value ||
-    !nberQuantity ||
-    !city ||
-    !condition
-    ) {
-      return false;
+// validation on change, so it mark the error directly
+formFirst.addEventListener("change", changeFormFirst);
+function changeFormFirst() {
+  console.log("Change Form First");
+  var name = /^[A-Z]{1}[a-zA-Z'À-ÿ -]+$/;
+  if (!formFirst.value.match(name)) {
+    formFirst.style.border = "red 2px solid";
+    formError[0].innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    FirstNIsValid = false;
+  } else {
+    formFirst.style.border = "none";
+    formError[0].innerHTML = "";
+    FirstNIsValid = true;
   }
-  return true;
+  validate();
 }
 
-//vérification champs remplis
-validation.addEventListener('submit', function(e) {
-  console.log
-  //empêche la soumission par défaut
-    e.preventDefault(); 
-    if (!isFormValid()) {
-      errorSubmit.innerHTML = "Veuillez renseigner tous les champs";
-      errorSubmit.style.color = "red";
-      return false;
+formLast.addEventListener("change", changeFormLast);
+function changeFormLast() {
+  console.log("Change Form Last");
+  var name = /^[A-Z]{1}[a-zA-Z'À-ÿ -]+$/;
+  if (!formLast.value.match(name)) {
+    formLast.style.border = "red 2px solid";
+    formError[1].innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    LastNIsValid = false;
+  } else {
+    formLast.style.border = "none";
+    formError[1].innerHTML = "";
+    LastNIsValid = true;
+  }
+}
+
+formMail.addEventListener("change", changeFormMail);
+function changeFormMail() {
+  console.log("Change Form Mail");
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (formMail.value.match(mailFormat)) {
+    formMail.style.border = "none";
+    formError[2].innerHTML = "";
+    MailIsValid = true;
+  } else {
+    formMail.style.border = "red 2px solid";
+    formError[2].innerHTML = "Cette adresse mail est incorrecte.";
+    MailIsValid = false;
+  }
+  validate();
+}
+
+formDate.addEventListener("change", changeFormDate);
+
+function changeFormDate() {
+  console.log("Change Form Date");
+  DateValid();
+
+  function DateValid() {
+    let val = formDate.value;
+    let dateParts = val.split("/");
+    
+    // Assurez-vous que nous avons trois parties (jour, mois, année)
+    if (dateParts.length !== 3) {
+      DateIsValid = false;
     } else {
-      message()
-      return true;
-    }
-  })
+      let day = parseInt(dateParts[0]);
+      let month = parseInt(dateParts[1]);
+      let year = parseInt(dateParts[2]);
 
-  //masque formulaire + message validation réussie
-  function message() {
-    //Masquer le formulaire d'origine
-    validation.className = 'hiddenModal';
-    //Afficher le message et le bouton de fermeture
-    thankMsg.className = 'showModal';
-  };
+      // Vérifiez si ce sont des nombres valides
+      if (isNaN(day) || isNaN(month) || isNaN(year)) {
+        DateIsValid = false;
+      } else {
+        // Vérifiez si la date est valide
+        let maxDay = new Date(year, month, 0).getDate();
+        DateIsValid = day > 0 && day <= maxDay && month > 0 && month <= 12 && year > 0;
+
+        // Affichez le message d'erreur si la date n'est pas valide
+        if (!DateIsValid) {
+          formDate.style.border = "red 2px solid";
+          formError[3].innerHTML = "Vous devez entrer une date de naissance valide au format JJ/MM/AAAA.";
+        } else {
+          formDate.style.border = "none";
+          formError[3].innerHTML = "";
+        }
+      }
+    }
+  }
+  validate();
+}
+
   
 
-  thankBtn.addEventListener('click', function event() {
-    //Réactiver le formulaire d'origine
-    validation.className = 'showModal';
-    //Masquer le message
-    thankMsg.className = 'hiddenModal';
-    // forcer la fermuture de la modale
-    closeModal();
-    //Réinitialiser le formulaire
-    validation.reset();
-    
-    
-  });
 
+formTimes.addEventListener("change", changeFormTimes);
+function changeFormTimes() {
+  console.log("Change Form Times");
+  TimesValid();
+  function TimesValid() {
+    let val = formTimes.value;
+    let valNumber = parseInt(val);
+    if (isNaN(valNumber)) {
+      TimesIsValid = false;
+      formTimes.style.border = "red 2px solid";
+      formError[4].innerHTML = "Veuillez entrer un chiffre.";
+    } else {
+      if (valNumber < 0) {
+        TimesIsValid = false;
+        formTimes.style.border = "red 2px solid";
+        formError[4].innerHTML = "Chiffre ne peut pas être inférieur à 0.";
+      } else TimesIsValid = true;
+      formTimes.style.border = "none";
+      formError[4].innerHTML = "";
+    }
+  }
+}
 
+function changeFormCity() {
+  console.log("Change Form City");
+  var formCitySelected = false;
+  for (let i = 0; i < formCity.length; i++) {
+    if (formCity[i].checked == true) formCitySelected = true;
+  }
+  if (formCitySelected == false) {
+    formError[5].innerHTML = "Vous devez choisir une option.";
+    CityIsValid = false;
+  }
+  if (formCitySelected == true) {
+    formError[5].innerHTML = "";
+    CityIsValid = true;
+  }
+  validate();
+}
+
+formCondition.addEventListener("change", changeFormCondition);
+function changeFormCondition() {
+  console.log("Change Form Condition");
+  if (formCondition.checked == false) {
+    formError[6].innerHTML =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
+    ConditionIsValid = false;
+  }
+  if (formCondition.checked == true) {
+    formError[6].innerHTML = "";
+    ConditionIsValid = true;
+  }
+  validate();
+}
